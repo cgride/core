005f5425387d33c24586eccd66551cf1cc9d199b
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-16 15:01:48 +0300

    Add strict cross-platform CI

---
+++ include/cgride/core.hpp
@@ -0,0 +1,30 @@
+/**
+ *
+ *  @file core.hpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2026, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/cgride/core
+ *
+ *  Use of this source code is governed by an MIT license
+ *  that can be found in the LICENSE file.
+ *
+ *  Cgride
+ *
+ */
+#ifndef CGRIDE_CORE_HPP
+#define CGRIDE_CORE_HPP
+
+#include <cgride/core/cancellation.hpp>
+#include <cgride/core/command.hpp>
+#include <cgride/core/diagnostic.hpp>
+#include <cgride/core/error.hpp>
+#include <cgride/core/event.hpp>
+#include <cgride/core/hash.hpp>
+#include <cgride/core/path.hpp>
+#include <cgride/core/platform.hpp>
+#include <cgride/core/result.hpp>
+#include <cgride/core/version.hpp>
+
+#endif // CGRIDE_CORE_HPP
