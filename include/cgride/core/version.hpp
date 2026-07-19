2f6d1996d33ec8a6a9d5f77704fb4f95a25c1e1a
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-19 13:27:43 +0300

    Bump version to 0.1.1

--- include/cgride/core/version.hpp
+++ include/cgride/core/version.hpp
@@ -33,7 +33,7 @@ namespace cgride::core
   /**
    * @brief Patch version of the Cgride core module.
    */
-  inline constexpr int version_patch = 0;
+  inline constexpr int version_patch = 1;
 
   /**
    * @brief Public API version of the Cgride core module.
@@ -43,7 +43,7 @@ namespace cgride::core
   /**
    * @brief Human-readable version string.
    */
-  inline constexpr std::string_view version_string = "0.1.0";
+  inline constexpr std::string_view version_string = "0.1.1";
 
 } // namespace cgride::core
 
